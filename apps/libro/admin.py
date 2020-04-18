990e28f47e08b19e69f14a31a091db532ca84925
AlejandroJose <alejandrohachiko2018@gmail.com>
2020-04-17 21:41:52 -0500

    'deploy'

---
+++ apps/libro/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Autor, Libro
+
+# Register your models here.
+
+admin.site.register(Autor)
+admin.site.register(Libro)
+
+
+
---
+++ apps/libro/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import Home, crearAutor, listarAutor, editarAutor, eliminarAutor
+
+urlpatterns = [
+    path('', Home, name='index'),
+    path('crear_autor', crearAutor, name='crear_autor'),
+    path('listar_autor/', listarAutor, name= 'listar_autor'),
+    path('editar_autor/<int:id>', editarAutor, name= 'editar_autor'),
+    path('eliminar_autor/<int:id>', eliminarAutor, name= 'eliminar_autor'),
+]
